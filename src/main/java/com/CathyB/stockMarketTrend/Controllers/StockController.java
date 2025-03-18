425b5aef9837bc2624374ebd24382b6976a07db2
Cathy Bastareaud <catybastareaud@gmail.com>
2025-03-18 10:49:25 -0400

    update pom/ add Finhub api/ modify StockService and StockController

--- src/main/java/com/CathyB/stockMarketTrend/Controllers/StockController.java
+++ src/main/java/com/CathyB/stockMarketTrend/Controllers/StockController.java
@@ -4,12 +4,12 @@ package com.CathyB.stockMarketTrend.Controllers;
 import com.CathyB.stockMarketTrend.Model.Stock;
 import com.CathyB.stockMarketTrend.Model.User;
 import com.CathyB.stockMarketTrend.Repository.StockRepository;
-import com.CathyB.stockMarketTrend.Repository.StockTrackingRepository;
 import com.CathyB.stockMarketTrend.Repository.UserRepository;
 import com.CathyB.stockMarketTrend.Service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,35 +21,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class StockController {
 
 
-  @Autowired
-  private StockService stockService;
+  private final StockService stockService;
+  private final UserRepository userRepository;
+  private final StockRepository stockRepository;
 
   @Autowired
-  private UserRepository userRepository;
+  public StockController(StockService stockService, UserRepository userRepository,
+      StockRepository stockRepository) {
+    this.stockService = stockService;
+    this.userRepository = userRepository;
+    this.stockRepository = stockRepository;
+  }
 
-  @Autowired
-  private StockRepository stockRepository;
 
-  @Autowired
-  private StockTrackingRepository stockTrackingRepository;
-
+  @GetMapping("/{ticker}")
+  public String getStockData(@PathVariable String ticker) {
+    return stockService.getStockData(ticker);
+  }
 
 
   @PostMapping("/add")
   @PreAuthorize("isAuthenticated()")
-public Stock addStock(@RequestBody Stock stock){
+  public Stock addStock(@RequestBody Stock stock) {
     System.out.println(SecurityContextHolder.getContext().getAuthentication());
     return stockService.addOrUpdateStock(stock);
   }
 
 
-
   @PostMapping("/track/{userId}/{ticker}")
   @PreAuthorize("isAuthenticated()")
-  public String trackStock(@PathVariable Long userId, @PathVariable String ticker){
+  public String trackStock(@PathVariable Long userId, @PathVariable String ticker) {
     System.out.println(SecurityContextHolder.getContext().getAuthentication() + "dkkdkdkdkdk");
     User user = userRepository.findById(userId)
-        .orElseThrow(()-> new RuntimeException("User not found" ));
+        .orElseThrow(() -> new RuntimeException("User not found"));
     System.out.println(user.getUsername());
 
     Stock stock = stockRepository.findByTicker(ticker);
@@ -62,9 +66,9 @@ public Stock addStock(@RequestBody Stock stock){
 
   @PostMapping("/untrack/{userId}/{ticker}")
   @PreAuthorize("isAuthenticated()")
-  public String unTrackStock(@PathVariable Long userId, @PathVariable String ticker){
+  public String unTrackStock(@PathVariable Long userId, @PathVariable String ticker) {
     User user = userRepository.findById(userId)
-        .orElseThrow(()-> new RuntimeException("USer not found"));
+        .orElseThrow(() -> new RuntimeException("USer not found"));
 
     Stock stock = stockRepository.findByTicker(ticker);
     if (stock == null) {
--- src/main/java/com/CathyB/stockMarketTrend/Service/StockService.java
+++ src/main/java/com/CathyB/stockMarketTrend/Service/StockService.java
@@ -7,7 +7,10 @@ import com.CathyB.stockMarketTrend.Model.User;
 import com.CathyB.stockMarketTrend.Repository.StockRepository;
 import com.CathyB.stockMarketTrend.Repository.StockTrackingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class StockService {
@@ -18,6 +21,21 @@ public class StockService {
 
   @Autowired
   private StockTrackingRepository stockTrackingRepository;
+
+  @Value("${finnhub.api.key}")
+  private String apiKEy;
+
+  private final RestTemplate restTemplate = new RestTemplate();
+
+  public String getStockData (String ticker){
+    String url = UriComponentsBuilder.fromHttpUrl("https://finnhub.io/api/v1/quote")
+        .queryParam("symbol", ticker)
+        .queryParam("token", apiKEy)
+        .toUriString();
+
+    return restTemplate.getForObject(url, String.class);
+
+  }
 
   public Stock addOrUpdateStock(Stock stock){
     return stockRepository.save(stock);
